541f3105a223622be18652dd1f1f69ca55f76c38
ghost1382 <mohammed.bn884@gmail.com>
2023-03-12 19:34:14 +0100

    first commit

---
+++ src/widgets/layout/configurator.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Button,
+  IconButton,
+  Switch,
+  Typography,
+  Chip,
+} from "@material-tailwind/react";
+import {
+  useMaterialTailwindController,
+  setOpenConfigurator,
+  setSidenavColor,
+  setSidenavType,
+  setFixedNavbar,
+} from "@/context";
+
+
+
+export function Configurator() {
+  const [controller, dispatch] = useMaterialTailwindController();
+  const { openConfigurator, sidenavColor, sidenavType, fixedNavbar } =
+    controller;
+  const [stars, setStars] = React.useState(0);
+
+
+return(
+  <div/>
+  );
+}
+
+// Configurator.displayName = "/src/widgets/layout/configurator.jsx";
+
+export default Configurator;
